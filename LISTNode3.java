1af4c140f1b97cfab2c76addd0a6d656d8db3f5b
lslcyberli <2305689151@qq.com>
2022-05-19 23:44:09 +0800

    5.19 solution

--- LISTNode3.java
+++ LISTNode3.java
@@ -441,3 +441,32 @@ class Solution {
         return answer;
     }
 }
+//4124370
+class Solution {
+    public int candy(int[] ratings) {
+        int len = ratings.length;
+        int[] left = new int[len];
+        int[] right = new int[len];
+        left[0] = 1;
+        right[len - 1] = 1;
+        for (int i = 1; i < len; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            } else {
+                left[i] = 1;
+            }
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                right[i] = right[i + 1] + 1;
+            } else {
+                right[i] = 1;
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < len; i++) {
+            res += Math.max(left[i], right[i]);
+        }
+        return res;
+    }
+}
