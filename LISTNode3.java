bbc7886a7192c050c55ed57876de3ff27bb984d6
lslcyberli <2305689151@qq.com>
2022-05-24 11:18:01 +0800

    5.24 solution

--- LISTNode3.java
+++ LISTNode3.java
@@ -633,3 +633,62 @@ class Solution {
         return profit;
     }
 }
+
+//4270330
+//编写一个函数来查找字符串数组中的最长公共前缀。
+//
+//如果不存在公共前缀，返回空字符串 ""。
+//
+// 
+//
+//示例 1：
+//
+//输入：strs = ["flower","flow","flight"]
+//输出："fl"
+//
+//示例 2：
+//
+//输入：strs = ["dog","racecar","car"]
+//输出：""
+//解释：输入不存在公共前缀。
+//
+// 
+//
+//提示：
+//
+//0 <= strs.length <= 200
+//0 <= strs[i].length <= 200
+//strs[i] 仅由小写英文字母组成
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        int i = 0;
+        StringBuilder lcp = new StringBuilder();
+        while (true) {
+            boolean done = false;
+            if (i >= strs[0].length()) {
+                break;
+            }
+            for (int j = 0; j < strs.length; j++) {
+                if (i < strs[j].length()) {
+                    if (strs[j].charAt(i) != strs[0].charAt(i)) {
+                        done = true;
+                        break;
+                    }
+                } else {
+                    done = true;
+                    break;
+                }
+            }
+            if (done) {
+                break;
+            } else {
+                lcp.append(strs[0].charAt(i));
+                i++;
+            }
+        }
+        return lcp.toString();
+    }
+}
