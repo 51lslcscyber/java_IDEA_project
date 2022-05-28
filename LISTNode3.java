227134998f0c471ecade9713c49415b02231fada
lslcyberli <2305689151@qq.com>
2022-05-28 10:56:36 +0800

    5.28 solution

--- LISTNode3.java
+++ LISTNode3.java
@@ -710,3 +710,37 @@ public class Egg {
         }
     }
 }
+
+//4331820
+//求1-100内的素数
+public static void main(String[] args){
+    for(int i=0;i<100;i++) {
+        checkPrime(i);
+    }
+}
+    private static void checkPrime(int x){
+        boolean isPrime = true;
+        if(x ==1 || x %2 ==0 && x !=2 )
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for( int i =3; i< Math.sqrt(x); i+=2)
+            {
+                if( x % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if( isPrime)
+        {
+            System.out.println(x +"是素数");
+        }
+        else
+        {
+            System.out.println(x+ "不是素数");
+        }
+    }
